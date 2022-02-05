a1cf834bba15201f88be46ae038fa7a2e6e4c063
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-02-05 12:21:47 +0300

    router query string, useRouter

--- pages/users.js
+++ pages/users.js
@@ -1,1 +1,14 @@
-export default () => <div>Users page</div>
+import {useRouter} from 'next/router';
+
+const Users = () => {
+    const router = useRouter();
+    return (
+        <div>
+            {JSON.stringify(router)}
+            <br/> <br/>
+            {JSON.stringify(router.query.name)}
+        </div>
+)
+};
+
+export default Users;
