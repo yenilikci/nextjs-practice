829eeac0b4fde6aaffcbebca26bbd02bec347dfc
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-02-05 12:09:38 +0300

    next components

--- pages/index.js
+++ pages/index.js
@@ -1,2 +1,10 @@
 //pages klasörü altında oluşturulan dosyalar url'leri ile çağırılır.
-export default () => <div>Index page</div>;
+
+import Navbar from '../components/Navbar';
+const  Home = () => (
+    <div>
+        <Navbar />
+        Home page
+    </div>);
+
+export default Home;
